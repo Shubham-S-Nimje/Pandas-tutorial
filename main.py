e34106125c24b0b1bc836347a7b0420a7042078f
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2024-09-27 15:29:27 +0530

    new

--- main.py
+++ main.py
@@ -26,7 +26,31 @@ poke = pd.read_csv('pokemon_data.csv')
 
 # poke['Total'] = poke.iloc[:, 4:10].sum(axis=1)
 
-cols = list(poke.columns)
-poke = poke[cols[0:4] + [cols[-1]]+cols[4:12]]
+# cols = list(poke.columns)
+# poke = poke[cols[0:4] + [cols[-1]]+cols[4:12]]
+
+# poke = pd.read_csv('modified.csv')
+
+# poke['count'] = 1
+
+# poke.groupby(['Type 1', 'Type 2']).count()['count']
+
+# poke.to_csv('modified.csv', index=False)
+
+#poke.to_excel('modified.xlsx', index=False)
+
+# poke.to_csv('modified.txt', index=False, sep='\t')
+
+# new_poke = poke.loc[(poke['Type 1'] == 'Grass') & (poke['Type 2'] == 'Poison') & (poke['HP'] > 70)]
+
+# new_poke.reset_index(drop=True, inplace=True)
+
+# new_poke.to_csv('filtered.csv')
+
+# import re
+
+# poke.loc[poke['Type 1'].str.contains('fire|grass', flags=re.I, regex=True)]
+
+# poke.loc[poke['Total'] > 500, ['Generation', 'Legendary']] = ['Test 1', 'Test 2']
 
 print(poke.head(5))
